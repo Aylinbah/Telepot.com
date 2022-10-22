3b9310f38df98fdcbc972dd42c80e703c7717010
Aylin <115853281+Aylinbah@users.noreply.github.com>
2022-10-22 13:55:13 +0800

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,5 @@
+import telepot 
+
+bot = telepot.Bot("5527673378:AAHDDAdEFs1nM5l4J6Hb2Sba0_2AUqfeE7I")
+
+bot.sendPhoto(chat_id="5420353293" , photo=("https://thecinemaholic.com/wp-content/uploads/2021/01/nezuu-e1638963260523.jpg"))
